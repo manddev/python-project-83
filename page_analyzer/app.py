9de98bef7df06d647dea3ed75562fab7830822b8
mandpc <malanddev@yandex.ru>
2026-04-26 21:29:22 +0300

    init page_analyzer

---
+++ page_analyzer/app.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+app.config['DEBUG'] = os.getenv('DEBUG') == 'True'
+
+
+
+@app.route("/")
+def index():
+    return "Hello, Page Analyzer!"
